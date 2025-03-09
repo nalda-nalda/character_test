1dd904bd3efd8ad376f6704cd67a488c8b7be087
gwangik <nalda0322@naver.com>
2025-03-10 08:43:02 +0900

    feat: HUD(경험치,레벨,킬)

--- Assets/Scripts/HUD.cs
+++ Assets/Scripts/HUD.cs
@@ -1,18 +1,44 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class HUD : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public enum InfoType { Exp, Level, Kill, Time, Health}
+    public InfoType type;
+
+    Text myText;
+    Slider mySlider;
+
+    void Awake()
     {
-        
+        myText = GetComponent<Text>();
+        mySlider = GetComponent<Slider>();
     }
 
-    // Update is called once per frame
-    void Update()
+    void LateUpdate()
     {
-        
+        switch(type){
+            case InfoType.Exp :
+                float curExp = GameManager.instance.exp;
+                float maxExp = GameManager.instance.nextExp[GameManager.instance.level];
+                mySlider.value = curExp / maxExp;
+                
+                break;
+            case InfoType.Level :
+                myText.text = string.Format("Lv.{0:F0}",GameManager.instance.level); 
+                break;
+            case InfoType.Kill :
+                myText.text = string.Format("{0:F0}",GameManager.instance.kill); 
+
+                break;
+            case InfoType.Time :
+
+                break;
+            case InfoType.Health :
+
+                break;
+        }
     }
 }
