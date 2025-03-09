7d9007d54567f6586e27769a464483f80a50bafe
gwangik <nalda0322@naver.com>
2025-03-09 17:50:39 +0900

    feat: 적 생성성

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -6,7 +6,7 @@ public class Enemy : MonoBehaviour
 {
     public float speed;
     public Rigidbody2D target;
-    bool isLive;
+    bool isLive= true;
 
     Rigidbody2D rigid;
     SpriteRenderer spriter;
@@ -17,10 +17,28 @@ public class Enemy : MonoBehaviour
     }
     
     void FixedUpdate(){
+        
+        if(!isLive) return;
+
         Vector2 dirVec = target.position - rigid.position;
         Vector2 nextVec = dirVec.normalized * speed * Time.fixedDeltaTime;
 
         rigid.MovePosition(rigid.position + nextVec);
         rigid.velocity = Vector2.zero;
+    }
+
+    void LateUpdate()
+    {
+        if(!isLive) return;
+
+        spriter.flipX = target.position.x < rigid.position.x;
+    }
+
+    /// <summary>
+    /// This function is called when the object becomes enabled and active.
+    /// </summary>
+    void OnEnable()
+    {
+        target = GameManager.instance.player.GetComponent<Rigidbody2D>();    
     }
 }
